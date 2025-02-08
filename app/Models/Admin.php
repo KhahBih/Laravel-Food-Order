eb7fcea8128e7ae1227dd62d987a8d73d96a4368
KhahBih <www.kbinh@gmail.com>
2025-02-08 23:09:11 +0700

    8-2

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -16,7 +16,7 @@ class Admin extends Authenticatable
      *
      * @var array<int, string>
      */
-     protected $guard = 'admin';
+     protected $guard = 'admina';
      protected $guarded = [];
 
     /**
