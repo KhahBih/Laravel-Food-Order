53aff2c3567f17812977ff7d0cd2a0422103d440
KhahBih <www.kbinh@gmail.com>
2025-02-18 21:46:42 +0700

    18-2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -53,5 +54,36 @@ class UserController extends Controller
      public function UserLogout(){
         Auth::guard('web')->logout();
         return redirect()->route('login')->with('success','Lougout Successfully');
+     }
+
+     public function ChangePassword(){
+        return view('frontend.dashboard.change_password');
+    }
+     // End Method
+
+     public function UserPasswordUpdate(Request $request){
+        $user = Auth::guard('web')->user();
+        $request->validate([
+            'old_password' => 'required',
+            'new_password' => 'required|confirmed'
+        ]);
+
+        if (!Hash::check($request->old_password,$user->password)) {
+            $notification = array(
+                'message' => 'Old Password Does not Match!',
+                'alert-type' => 'error'
+            );
+            return back()->with($notification);
+        }
+        /// Update the new password
+        User::whereId($user->id)->update([
+            'password' => Hash::make($request->new_password)
+        ]);
+
+                $notification = array(
+                'message' => 'Password Change Successfully',
+                'alert-type' => 'success'
+            );
+            return back()->with($notification);
      }
 }
