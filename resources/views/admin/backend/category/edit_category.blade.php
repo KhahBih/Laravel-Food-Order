5fc0d309bbc31ec57a88d20837ed063bcf5062e9
KhahBih <www.kbinh@gmail.com>
2025-02-21 22:08:57 +0700

    21-2

--- resources/views/admin/backend/category/edit_category.blade.php
+++ resources/views/admin/backend/category/edit_category.blade.php
@@ -9,12 +9,12 @@
         <div class="row">
             <div class="col-12">
                 <div class="page-title-box d-sm-flex align-items-center justify-content-between">
-                    <h4 class="mb-sm-0 font-size-18">Add Category</h4>
+                    <h4 class="mb-sm-0 font-size-18">Edit Category</h4>
 
                     <div class="page-title-right">
                         <ol class="breadcrumb m-0">
                             <li class="breadcrumb-item"><a href="javascript: void(0);">Dashboard</a></li>
-                            <li class="breadcrumb-item active">Add Category  </li>
+                            <li class="breadcrumb-item active">Edit Category  </li>
                         </ol>
                     </div>
 
@@ -24,39 +24,39 @@
         <!-- end page title -->
 
         <div class="row">
-            <div class="col-xl-9 col-lg-8">
+            <div class="col-xl-9 col-lg-8"> 
  <div class="card">
 <div class="card-body p-4">
 
-<form id="myForm" action="{{ route('category.store') }}" method="post" enctype="multipart/form-data">
+<form id="myForm" action="{{ route('category.update') }}" method="post" enctype="multipart/form-data">
     @csrf
-
+    <input type="hidden" name="id" value="{{ $category->id }}" >
 <div class="row">
     <div class="col-lg-12">
         <div>
             <div class="form-group mb-3">
                 <label for="example-text-input" class="form-label">Category Name</label>
-                <input class="form-control" type="text" name="category_name"  id="example-text-input">
+                <input class="form-control" type="text" name="category_name" value="{{ $category->category_name }}"  id="example-text-input">
             </div>
-
+ 
         </div>
     </div>
 
     <div class="col-lg-6">
         <div class="mt-3 mt-lg-0">
-
+            
             <div class="form-group mb-3">
                 <label for="example-text-input" class="form-label">Category Image</label>
                 <input class="form-control" name="image" type="file"  id="image">
             </div>
             <div class="mb-3">
-
-                <img id="showImage" src="{{ url('upload/no_image.jpg') }}" alt="" class="rounded-circle p-1 bg-primary" width="110">
+                 
+                <img id="showImage" src="{{ asset($category->image) }}" alt="" class="rounded-circle p-1 bg-primary" width="110">
             </div>
             <div class="mt-4">
                 <button type="submit" class="btn btn-primary waves-effect waves-light">Save Changes</button>
             </div>
-
+              
         </div>
     </div>
 </div>
@@ -72,16 +72,16 @@
 
 
 
-
+               
                 <!-- end tab content -->
             </div>
             <!-- end col -->
 
-
+            
             <!-- end col -->
         </div>
         <!-- end row -->
-
+        
     </div> <!-- container-fluid -->
 </div>
 
@@ -105,22 +105,18 @@
                 category_name: {
                     required : true,
                 },
-                image: {
-                    required : true,
-                },
-
+                 
+                
             },
             messages :{
                 category_name: {
                     required : 'Please Enter Category Name',
                 },
-                image: {
-                    required : 'Please Select Image',
-                },
-
+                
+                 
 
             },
-            errorElement : 'span',
+            errorElement : 'span', 
             errorPlacement: function (error,element) {
                 error.addClass('invalid-feedback');
                 element.closest('.form-group').append(error);
@@ -133,7 +129,7 @@
             },
         });
     });
-
+    
 </script>
 
 
